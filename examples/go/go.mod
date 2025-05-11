07ed116c2f553e9f6c53d7358cd37e3b270cc1b6
mbrav <mbrav@protonmail.com>
2025-05-11 13:23:27 +0300

    change to provider builder

--- examples/go/go.mod
+++ examples/go/go.mod
@@ -3,7 +3,7 @@ module provider-netbird
 go 1.24
 
 require (
-	github.com/mbrav/pulumi-netbird/sdk v0.0.8
+	github.com/mbrav/pulumi-netbird/sdk v0.0.9
 	github.com/pulumi/pulumi/sdk/v3 v3.169.0
 )
 
--- provider/provider.go
+++ provider/provider.go
@@ -3,78 +3,80 @@ package provider
 import (
 	p "github.com/pulumi/pulumi-go-provider"
 	"github.com/pulumi/pulumi-go-provider/infer"
-	"github.com/pulumi/pulumi-go-provider/middleware/schema"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
 )
 
 // Change to const to disable semver Version management
 var (
 	Name    string = "netbird"
-	Version string = "0.0.8"
+	Version string = "0.0.9"
 )
 
 // Define Provider.
 func Provider() p.Provider {
-	return infer.Provider(infer.Options{
-		Metadata: schema.Metadata{
-			DisplayName: Name,
-			Description: "Manage NetBird resources declaratively using Pulumi's infrastructure-as-code framework.",
-			Keywords: []string{
-				"pulumi",
-				"networking",
-				"netbird",
-				"security",
-			},
-			Homepage:          "https://pulumi.com",
-			License:           "AGPL-3.0",
-			Repository:        "https://github.com/mbrav/pulumi-netbird",
-			PluginDownloadURL: "github://api.github.com/mbrav",
-			Publisher:         "mbrav",
-			LogoURL:           "https://raw.githubusercontent.com/mbrav/pulumi-netbird/master/assets/logo.webp",
-			// This contains language specific details for generating the provider's SDKs
-			LanguageMap: map[string]any{
-				"csharp": map[string]any{
-					"packageReferences": map[string]string{
-						"Pulumi": "3.*",
-					},
-				},
-				"go": map[string]any{
-					"generateResourceContainerTypes": true,
-					"importBasePath":                 "github.com/mbrav/pulumi-netbird/sdk/go/netbird",
-				},
-				"nodejs": map[string]any{
-					"packageName": "@mbrav/pulumi-netbird",
-					"dependencies": map[string]string{
-						"@pulumi/pulumi": "^3.0.0",
-					},
-				},
-				"python": map[string]any{
-					"requires": map[string]string{
-						"pulumi": ">=3.0.0,<4.0.0",
-					},
-				},
-				"java": map[string]any{
-					"buildFiles":                      "gradle",
-					"gradleNexusPublishPluginVersion": "2.0.0",
-					"dependencies": map[string]any{
-						"com.pulumi:pulumi": "1.10.0",
-					},
-				},
-			},
-		},
-		Components: []infer.InferredComponent{},
-		Resources: []infer.InferredResource{
+	// Build provider
+	provider, err := infer.NewProviderBuilder().
+		WithDisplayName(Name).
+		WithDescription("Manage NetBird resources declaratively using Pulumi's infrastructure-as-code framework.").
+		WithKeywords(
+			"pulumi",
+			"networking",
+			"netbird",
+			"security",
+		).
+		WithHomepage("https://pulumi.com").
+		WithLicense("AGPL-3.0").
+		WithRepository("https://github.com/mbrav/pulumi-netbird").
+		WithPluginDownloadURL("github://api.github.com/mbrav").
+		WithPublisher("mbrav").
+		WithLogoURL("https://raw.githubusercontent.com/mbrav/pulumi-netbird/master/assets/logo.webp").
+		// WithNamespace("nb").
+		WithConfig(infer.Config[*Config]()).
+		WithResources(
 			infer.Resource[*Group](),
 			infer.Resource[*Network](),
 			infer.Resource[*NetworkResource](),
 			infer.Resource[*NetworkRouter](),
 			infer.Resource[*Peer](),
 			infer.Resource[*Policy](),
-		},
-		Functions: []infer.InferredFunction{},
-		// Config:    infer.Config[*Config](),
-		ModuleMap: map[tokens.ModuleName]tokens.ModuleName{
+		).
+		WithModuleMap(map[tokens.ModuleName]tokens.ModuleName{
 			"auto-naming": "index",
-		},
-	})
+		}).
+		WithLanguageMap(map[string]any{
+			"csharp": map[string]any{
+				"packageReferences": map[string]string{
+					"Pulumi": "3.*",
+				},
+			},
+			"go": map[string]any{
+				"generateResourceContainerTypes": true,
+				"importBasePath":                 "github.com/mbrav/pulumi-netbird/sdk/go/netbird",
+			},
+			"nodejs": map[string]any{
+				"packageName": "@mbrav/pulumi-netbird",
+				"dependencies": map[string]string{
+					"@pulumi/pulumi": "^3.0.0",
+				},
+			},
+			"python": map[string]any{
+				"requires": map[string]string{
+					"pulumi": ">=3.0.0,<4.0.0",
+				},
+			},
+			"java": map[string]any{
+				"buildFiles":                      "gradle",
+				"gradleNexusPublishPluginVersion": "2.0.0",
+				"dependencies": map[string]any{
+					"com.pulumi:pulumi": "1.10.0",
+				},
+			},
+		}).
+		Build()
+		// Check error
+	if err != nil {
+		panic("failed to build provider: " + err.Error())
+	}
+
+	return provider
 }

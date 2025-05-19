07c59a682b0ce9dc37a6a471822d8c39f12e071c
mbrav <mbrav@protonmail.com>
2025-05-19 19:48:09 +0300

    Remove component for now

--- provider/provider.go
+++ provider/provider.go
@@ -1,7 +1,6 @@
 package provider
 
 import (
-	"github.com/mbrav/pulumi-netbird/provider/component"
 	"github.com/mbrav/pulumi-netbird/provider/config"
 	"github.com/mbrav/pulumi-netbird/provider/resource"
 	p "github.com/pulumi/pulumi-go-provider"
@@ -45,11 +44,9 @@ func Provider() p.Provider {
 			infer.Resource(&resource.Peer{}),
 			infer.Resource(&resource.Policy{}),
 		).
-		WithComponents(
-			infer.ComponentF(component.NewACLFileComponent),
-			// infer.ComponentF(&component.ACLFile{}),
-			// infer.Component(&component.ACLFile{}),
-		).
+		// WithComponents(
+		// 	infer.ComponentF(component.NewACLFileComponent),
+		// ).
 		// WithFunctions(functions ...infer.InferredFunction)
 		WithModuleMap(map[tokens.ModuleName]tokens.ModuleName{
 			"auto-naming": "index",

ed59822f457b657fc43e9115ecc63e5c05ebff02
mbrav <mbrav@protonmail.com>
2025-07-15 16:16:34 +0300

    bump

--- provider/provider.go
+++ provider/provider.go
@@ -13,7 +13,7 @@ import (
 // Change to var to enable semver Version management.
 const (
 	Name    string = "netbird"
-	Version string = "0.0.27"
+	Version string = "0.0.28"
 )
 
 // Provider creates and returns a new Pulumi provider for managing NetBird resources.
